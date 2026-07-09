26bcd54822fe0a22359935fa17538e32f6460096
DiogoEsteves <diogoestevesmorgado@gmail.com>
2026-07-09 20:22:48 +0100

    Add working copy view backed by TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useAppStore } from "./state/appStore";
 import { OpenRepo } from "./features/repo/OpenRepo";
+import { WorkingCopy } from "./features/working-copy/WorkingCopy";
 
 export default function App() {
   const repo = useAppStore((s) => s.repo);
   if (!repo) return <OpenRepo />;
-  return (
-    <div style={{ padding: 24 }}>
-      <div>Repo: {repo.path}</div>
-      <div>Branch: {repo.current_branch}</div>
-    </div>
-  );
+  return <WorkingCopy />;
 }
